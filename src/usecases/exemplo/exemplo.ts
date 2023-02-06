f50758607fedf3443bf2181308f80bd927297ca1
Fernando Carvalho <fernando.carvalho.iff@gmail.com>
2023-02-06 09:31:49 -0300

    fix: quebrei o build, foi mal, galera!

--- src/usecases/exemplo/exemplo.ts
+++ src/usecases/exemplo/exemplo.ts
@@ -1,7 +1,7 @@
 export class Exemplo {
     async perform(params: Exemplo.Params): Promise<Exemplo.Result> {
         const retorno: Exemplo.Result = {
-            saida: ''
+            saida: 'resultado'
         }
         return retorno
     }
