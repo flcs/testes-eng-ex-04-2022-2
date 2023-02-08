6b123b0100a6d57d8150ca0f59264584f7910e54
Weriks Santos <werikscs@gmail.com>
2023-02-08 17:05:46 -0300

    test: consult processing compiling

--- src/usecases/solicitation/consultProcessing/consult-processing.spec.ts
+++ src/usecases/solicitation/consultProcessing/consult-processing.spec.ts
@@ -1,9 +1,33 @@
+import { InMemorySolicitationRepository, IRepositorySolicitation } from '@/repositories/inMemory/inmemory-solicitation-repository'
+import IUseCase from '@/usecases/iusecase'
+
+type ConsultProcessingInput = {
+  solicitationID: string
+}
+
+type ConsultProcessingOutput = {
+  solicitationCalculation: number
+}
+
+
+class ConsultProcessing implements IUseCase<ConsultProcessingInput,ConsultProcessingOutput> {
+  private repository: IRepositorySolicitation
+
+  constructor(repository: IRepositorySolicitation) {
+    this.repository = repository
+  }
+
+  perform(input: ConsultProcessingInput): Promise<ConsultProcessingOutput> {
+    throw new Error('Method not implemented.')
+  }
+}
+
 describe('Use Case - Consultar Processamento', () => {
   it('deveria consultar o processamento de forma correta', async () => {
     const inMemorySolicitationRepository = new InMemorySolicitationRepository()
     const sut = new ConsultProcessing(inMemorySolicitationRepository)
     const input = { solicitationID: '0' }
     const output = await sut.perform(input)
-    expect(output.calculationResult).toBe(1000)
+    expect(output.solicitationCalculation).toBe(1000)
   })
 })
