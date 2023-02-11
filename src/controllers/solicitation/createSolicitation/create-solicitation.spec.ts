e9df9a1bbd4933e206968c80125b4a1a18af9739
Weriks Santos <werikscs@gmail.com>
2023-02-10 22:09:04 -0300

    test(create-solicitation-controller): test returning 400 when input is not valid

--- src/controllers/solicitation/createSolicitation/create-solicitation.spec.ts
+++ src/controllers/solicitation/createSolicitation/create-solicitation.spec.ts
@@ -20,10 +20,23 @@ class CreateSolicitationController implements IController<CreateSolicitationRequ
   constructor(private readonly createSolictitationUseCase: CreateSolicitation){}
 
   async handle(request: CreateSolicitationRequestData): Promise<ResponseData> {
+    if(!this.validate(request.title, request.cost)) {
+      return {
+        statusCode: 400,
+        body: { message: "Invalid input" }
+      }
+    }
     return {
       statusCode: 201,
       body: {}
     }
+  }
+
+  private validate(title: string, cost: number): boolean {
+    const isTitleLengthGreaterThanThree = title.trim().length > 3
+    const titleContainsAtLeastOneLetter = Boolean(title.trim().match(/[A-z]/g))
+    const isCostGreaterThanZero = cost > 0
+    return isTitleLengthGreaterThanThree && titleContainsAtLeastOneLetter && isCostGreaterThanZero
   }
 }
 
@@ -35,5 +48,15 @@ describe('Controller - Criar Solicitação', () => {
     const request = { title: "Reparar porta", cost: 1000 }
     const response = await sut.handle(request)
     expect(response.statusCode).toBe(201)
+  })
+
+  it('deveria retornar 400 e uma mensagem do erro, se os dados não forem válidos', async () => {
+    const inMemorySolicitationRepository = new InMemorySolicitationRepository()
+    const createSolicitationUseCase = new CreateSolicitation(inMemorySolicitationRepository)
+    const sut = new CreateSolicitationController(createSolicitationUseCase)
+    const request = { title: "", cost: 1000 }
+    const response = await sut.handle(request)
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toHaveProperty("message")
   })
 })
