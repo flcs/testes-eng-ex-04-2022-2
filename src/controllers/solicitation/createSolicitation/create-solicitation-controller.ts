9eb54acdc82b06746f54da390b064dd54bbe7d94
Weriks Santos <werikscs@gmail.com>
2023-02-15 18:19:04 -0300

    style: change formatting

--- src/controllers/solicitation/createSolicitation/create-solicitation-controller.ts
+++ src/controllers/solicitation/createSolicitation/create-solicitation-controller.ts
@@ -1,14 +1,16 @@
 import { IController, ResponseData } from '@/controllers/icontroller'
 import CreateSolicitation from '@/usecases/solicitation/createSolicitation/create-solicitation'
 import CreateSolicitationRequest from './create-solicitation-controller-request'
-import { InvalidInputError } from './errors'
+import { InvalidInputError } from '../../errors'
 
 class CreateSolicitationController implements IController<CreateSolicitationRequest> {
   constructor(private readonly createSolictitationUseCase: CreateSolicitation) { }
 
   async handle(request: CreateSolicitationRequest): Promise<ResponseData> {
     try {
-      const output = await this.createSolictitationUseCase.perform({ title: request.title, cost: request.cost })
+      const output = await this.createSolictitationUseCase.perform({
+         title: request.title, cost: request.cost
+      })
       return { statusCode: 201, body: { ...output } }
     } catch (error) {
       if (error instanceof InvalidInputError) {
